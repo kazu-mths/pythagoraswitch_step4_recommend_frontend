501935307000125ec1c92699d4361fdc63aaabeb
kazu-mths <123311255+kazu-mths@users.noreply.github.com>
2024-03-13 03:24:10 +0900

    Update QrcodeCashRegister4.tsx

--- src/app/components/Qrcode/QrcodeCashRegister4.tsx
+++ src/app/components/Qrcode/QrcodeCashRegister4.tsx
@@ -71,9 +71,8 @@ export default function QrcodeReaderComponent() {
 
 useEffect(() => {
     const tokenFromQuery: string | string[] | undefined = router.query.token;
-    const user_token: string | null = typeof tokenFromQuery === 'string' ? tokenFromQuery : null;
-    if (user_token) {
-        setToken(user_token);
+    if (tokenFromQuery) {
+        setToken(tokenFromQuery);
     }
 }, [router.query.token]);
     
