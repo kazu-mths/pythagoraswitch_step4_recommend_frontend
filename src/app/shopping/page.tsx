919ae0876596369dde05b2613d9bca944c17e934
Kazu <tokai.kazuma@gmail.com>
2024-03-13 00:10:06 +0900

    initial

---
+++ src/app/shopping/page.tsx
@@ -0,0 +1,13 @@
+"use client";// import先は適宜置き換えてください。
+import React from 'react';
+import QrcodeCashRegister4 from './../components/Qrcode/QrcodeCashRegister4';
+
+export default function Shopping() {
+  return (
+      <main>
+        <div>
+          <QrcodeCashRegister4 />
+        </div>
+      </main>
+  );
+}
