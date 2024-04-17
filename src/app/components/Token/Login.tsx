2e804afe69c2371094fb14d9cbbc5802e31b0fbe
kazu-mths <123311255+kazu-mths@users.noreply.github.com>
2024-04-18 02:43:16 +0900

    Update Login.tsx

--- src/app/components/Token/Login.tsx
+++ src/app/components/Token/Login.tsx
@@ -19,7 +19,7 @@ export default function Login() {
                 password: formData.get('password'),
             });
 
-            const response = await fetch('http://127.0.0.1:8000/login', {
+            const response = await fetch('https://tech0-gen-5-step4-studentwebapp-1.azurewebsites.net/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
