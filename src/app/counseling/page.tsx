0d8ef2e4a5ecc34c403523125d0fd173e16128b5
kazu-mths <123311255+kazu-mths@users.noreply.github.com>
2024-04-20 02:40:49 +0900

    Update page.tsx

--- src/app/counseling/page.tsx
+++ src/app/counseling/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import counseling from './../components/Qrcode/Counseling';
 
-export default function Counseling() {
+export default function CounselingPage() {
   return (
       <main>
         <div>
